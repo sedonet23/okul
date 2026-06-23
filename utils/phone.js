3e3019ac2b6b5a358b744d45091027dee12b4535
sedonet23 <sedonet23@gmail.com>
2026-06-23 19:59:59 +0300

    Delete utils directory

--- utils/phone.js
+++
@@ -1,5 +0,0 @@
-export function callPhone(number) {
-  if (!number) return;
-  const cleanNumber = number.replace(/\s/g, '');
-  window.location.href = `tel:${cleanNumber}`;
-}
--- utils/whatsapp.js
+++
@@ -1,6 +0,0 @@
-export function openWhatsApp(number, message = '') {
-  if (!number) return;
-  const clean = number.replace(/\D/g, '');
-  const url = `https://wa.me/90${clean}?text=${encodeURIComponent(message)}`;
-  window.open(url, '_blank');
-}
