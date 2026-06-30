474c2f7ce2a1234ae226aaafd584d519c1ee1fbb
sedonet23 <sedonet23@gmail.com>
2026-06-30 22:36:11 +0300

    Add files via upload

--- js/service-worker.js
+++ js/service-worker.js
@@ -5,7 +5,7 @@
    · Strateji: statik dosyalar "Cache First", dış kaynaklar "Network First"
    ==================================================================== */
 
-const CACHE_ADI = 'oy-cache-v5';
+const CACHE_ADI = 'oy-cache-v6';
 
 /* ---- Önbelleğe alınacak tüm uygulama dosyaları ---- */
 const ONBELLEGE_ALINACAKLAR = [
