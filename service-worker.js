908e660e8a130a2385a526e7bd5e7a9d5fa6cb00
sedonet23 <sedonet23@gmail.com>
2026-07-16 23:58:05 +0300

    Update cache name from oy-cache-v241 to oy-cache-v242

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    · Strateji: statik dosyalar "Cache First", dış kaynaklar "Network First"
    ==================================================================== */
 
-const CACHE_ADI = 'oy-cache-v241';
+const CACHE_ADI = 'oy-cache-v242';
 
 /* ---- Önbelleğe alınacak tüm uygulama dosyaları ---- */
 const ONBELLEGE_ALINACAKLAR = [
