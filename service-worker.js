ee8277b92ba0f5f82170a70e48cdd492f24ebc4f
sedonet23 <sedonet23@gmail.com>
2026-07-20 23:14:23 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    · Strateji: statik dosyalar "Cache First", dış kaynaklar "Network First"
    ==================================================================== */
 
-const CACHE_ADI = 'oy-cache-v261';
+const CACHE_ADI = 'oy-cache-v262';
 
 /* ---- Önbelleğe alınacak tüm uygulama dosyaları ---- */
 const ONBELLEGE_ALINACAKLAR = [
